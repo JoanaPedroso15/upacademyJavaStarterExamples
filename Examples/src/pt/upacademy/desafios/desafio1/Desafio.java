de2ec79ca4621362b1db3fc013636414c6c7bf9e
JoanaPedroso <joana.pedroso15@gmail.com>
2019-12-12 15:07:31 +0000

    add resu

--- Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
@@ -1,24 +1,34 @@
 package pt.upacademy.desafios.desafio1;
 
 public class Desafio {
+	private int count;
+	private int Big3;
+	private int Big5;
 
 	public Desafio(int i) {
-		// TODO Auto-generated constructor stub
+		this.count = i;
 	}
 
 	public void run() {
-		// TODO Auto-generated method stub
+		for (int index =1; index <= count; index++) {
+			if (index % 3 == 0 && index % 5 ==0) {
+				Big3++;
+				Big5++;
+			} else if (index % 3 == 0) {
+				Big3++;
+			} else if (index % 5 == 0) {
+				Big5++;
+			}
+		}
 		
 	}
 
 	public int getBig3Count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Big3;
 	}
 
 	public int getBig5Count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Big5;
 	}
 
 }
