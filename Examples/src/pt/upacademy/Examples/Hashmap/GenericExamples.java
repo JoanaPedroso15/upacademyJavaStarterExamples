46d5f7f9828e53a2ac156d6deb0b747a1655f46f
JoanaPedroso <joana.pedroso15@gmail.com>
2019-12-13 18:11:31 +0000

    hashmap update, generic ex

---
+++ Examples/src/pt/upacademy/Examples/Hashmap/GenericExamples.java
@@ -0,0 +1,25 @@
+package pt.upacademy.Examples.Hashmap;
+
+public class GenericExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+	
+class Entity {
+	
+}
+
+class A extends Entity {
+	
+}
+
+class EntityRep <T extends Entity> {
+	
+}
+
+class ARep extends EntityRep <A> {
+	
+}
--- Examples/src/pt/upacademy/Examples/Hashmap/HashMapExample.java
+++ Examples/src/pt/upacademy/Examples/Hashmap/HashMapExample.java
@@ -1,24 +1,55 @@
 package pt.upacademy.Examples.Hashmap;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class HashMapExample {
 
-		Map <Long, Product> myMap = new HashMap <Long, Product>();
+		Map <Long, Product> myMap = new HashMap <Long, Product>(); // para trabalhar num hashmap tenho que por uma key e uma value; a key deve ser unica
 		
 		public static void main(String [] args) {
 			HashMapExample myapp = new HashMapExample();
-			myapp.run();
+			Product p1 = new Product("Coca-cola");
+			myapp.addProduct(p1);
+			//System.out.println(p1);
+			Product p2 = new Product("Batatas");
+			//System.out.println(p2);
+			myapp.addProduct(p2);
+			//System.out.println(myapp.getAll());
+			//myapp.removeProduct(p1);
+			//System.out.println(myapp.getAllIds());
+			myapp.printAll();
 			
 		}
 		
-		public void run () {
-			Product p1 = new Product("Coca-cola");
-			System.out.println(p1);
-			Product p2 = new Product("batatas");
-			System.out.println(p2);
-			myMap.put (p1.getId(),p1);
+		public void addProduct (Product p) {
+			myMap.put (p.getId(),p); //metodo put envia a key de identificacao 
+		}
+		
+		public void removeProduct (Product p) {
+			myMap.remove(p.getId(), p);
+		}
+		
+		public boolean isEmpty () {
+			return (myMap.size() == 0) ? true:false; //metodo size indica o tamanho do HashMap
+		}
+		
+		public Collection<Product> getAll (){
+			return myMap.values(); // metodo values retorna todos valores que existem 
+		}
+		
+		public Collection<Long> getAllIds (){
+			return myMap.keySet();
+		}
+		
+		public void printAll () {
+			Iterator<Product> prodIterator = getAll().iterator();
+			while (prodIterator.hasNext()) {
+				Product product = (Product) prodIterator.next();
+				System.out.println(product);
+			}
 		}
 	}
 
