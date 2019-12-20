3695ede2a6aee669426e4d6b44b200a72c499df4
JoanaPedroso <joana.pedroso15@gmail.com>
2019-12-20 20:47:00 +0000

    fix hashmap

--- Examples/src/pt/upacademy/Examples/HashMap/HashMapExample.java
+++ Examples/src/pt/upacademy/Examples/HashMap/HashMapExample.java
@@ -1,4 +1,4 @@
-package pt.upacademy.Examples.HashMap;
+package pt.upacademy.Examples.Hashmap;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -6,73 +6,75 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class HashMapExample {
-	
-	Map<Long,Product> myMap = new HashMap<Long, Product>();
-	
-	
-	public static void main(String[] args) {
-		HashMapExample myapp = new HashMapExample();
-		Product p1 = new Product("Coca Cola");
-//		System.out.println(p1);
-		myapp.addProduct(p1);
-		Product p2 = new Product("Batatas");
-//		System.out.println(p2);
-		myapp.addProduct(p2);
+
+		Map <Long, Product> myMap = new HashMap <Long, Product>(); // para trabalhar num hashmap tenho que por uma key e uma value; a key deve ser unica
 		
-//		System.out.println(myapp.getAll());
-//		myapp.removeProduct(p1);
-//		System.out.println(myapp.getAllIds());
-		myapp.printAll();
-	}
-	
-	public void addProduct(Product p) {
-		myMap.put(p.getId(), p);
-	}
-	
-	public void removeProduct(Product p) {
-		myMap.remove(p.getId(), p);
-	}
-	
-	public boolean isEmpty() {
-		return (myMap.size() == 0) ? true: false;
-	}
-	
-	public Collection<Product> getAll() {
-		return myMap.values();
-	}
-	
-	public Collection<Long> getAllIds() {
-		return myMap.keySet();
-	}
-	
-	public void printAll() {
-		Iterator<Product> prodInterator = getAll().iterator();
-		while (prodInterator.hasNext()) {
-			Product product = (Product) prodInterator.next();
-			System.out.println(product);
+		public static void main(String [] args) {
+			HashMapExample myapp = new HashMapExample();
+			Product p1 = new Product("Coca-cola");
+			myapp.addProduct(p1);
+			//System.out.println(p1);
+			Product p2 = new Product("Batatas");
+			//System.out.println(p2);
+			myapp.addProduct(p2);
+			//System.out.println(myapp.getAll());
+			//myapp.removeProduct(p1);
+			//System.out.println(myapp.getAllIds());
+			myapp.printAll();
+			
+		}
+		
+		public void addProduct (Product p) {
+			myMap.put (p.getId(),p); //metodo put envia a key de identificacao 
+		}
+		
+		public void removeProduct (Product p) {
+			myMap.remove(p.getId(), p);
+		}
+		
+		public boolean isEmpty () {
+			return (myMap.size() == 0) ? true:false; //metodo size indica o tamanho do HashMap
+		}
+		
+		public Collection<Product> getAll (){
+			return myMap.values(); // metodo values retorna todos valores que existem 
+		}
+		
+		public Collection<Long> getAllIds (){
+			return myMap.keySet();
+		}
+		
+		public void printAll () {
+			Iterator<Product> prodIterator = getAll().iterator();
+			while (prodIterator.hasNext()) {
+				Product product = (Product) prodIterator.next();
+				System.out.println(product);
+			}
 		}
 	}
-}
 
-class Product {
-	static long count = 1;
-	private long id;
-	private String name;
-	
-	public Product(String name) {
-		this.name = name;
-		this.id = count++;
-	}
-	
+
+	class Product {
+		static long count = 1;
+		private long id;
+		private String name;
+		
+		public Product(String name) {
+			this.name = name;
+			this.id = count++;
+		}
+		
 	public long getId() {
-		return this.id;
+			return this.id;
+		}
+
+		@Override
+		public String toString() {
+			return "Product [id=" + id + ", name=" + name + "]";
+		}
+		
 	}
 
-	@Override
-	public String toString() {
-		return "Product [id=" + id + ", name=" + name + "]";
-	}
 
-	
-	
-}
+
+
